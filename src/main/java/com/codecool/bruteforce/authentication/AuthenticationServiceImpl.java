21d0385ad09629999943bee5c8ec0d2fa7ae4187
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-07-05 16:21:51 +0300

    add the AuthenticationServiceImpl class

---
+++ src/main/java/com/codecool/bruteforce/authentication/AuthenticationServiceImpl.java
@@ -0,0 +1,8 @@
+package com.codecool.bruteforce.authentication;
+
+public class AuthenticationServiceImpl implements AuthenticationService{
+    @Override
+    public boolean authenticate(String userName, String password) {
+        return false;
+    }
+}
